9fca7015e508101e5401b941605a996bc926e2ab
IsaacDoong <isaac.doong@students.plymouth.ac.uk>
2026-03-17 18:38:54 +0000

    Small Update

--- IsaacDoong/model/bert_fine_tune.py
+++ IsaacDoong/model/bert_fine_tune.py
@@ -89,6 +89,7 @@ training_args = TrainingArguments(
     eval_strategy="epoch",
     save_strategy="epoch",
     logging_steps=5,
+    save_total_limit=1,
     load_best_model_at_end=True,
     metric_for_best_model="f1",
     greater_is_better=True,
